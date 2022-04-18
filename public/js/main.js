436f8c5489c00c51ad54261c087896141b7c37c4
ayatamine <ayatir04@gmail.com>
2022-04-19 00:49:07 +0100

    add the portfolio section

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,9 @@ function updateList() {
     const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
         return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
     });
-    console.log(titles)
-    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
 
+    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+    // console.log('yes amine', titles[0])
     document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
 }
 
