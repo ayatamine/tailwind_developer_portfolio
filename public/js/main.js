784f5a8bba294e1efaad015a4eb81b6b2b9299df
ayatamine <ayatir04@gmail.com>
2022-04-23 19:24:58 +0100

    setup scroll animation with gsap scrolltrigger

--- public/js/main.js
+++ public/js/main.js
@@ -46,4 +46,109 @@ function switchMenu(type) {
 //hide mobile menu when choose a link from it
 document.querySelectorAll('.mobile-menu li a').forEach(link => {
     link.addEventListener('click', () => switchMenu('close'))
-})
+});
+//animation on scroll
+gsap.registerPlugin(ScrollTrigger)
+gsap.to([".featured-img", ".caption"], {
+    scrollTrigger: {
+        trigger: ".about-me-section",
+        start: "top top",
+    },
+    delay: 0,
+    duration: 1.5,
+    opacity: 1,
+    right: '0%',
+    ease: 'Power3.in'
+        // ease: 'slow.easeInOut'
+});
+gsap.to([".resume-box .timeline"], {
+    scrollTrigger: {
+        trigger: ".resume-box",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 1,
+    opacity: 1,
+    y: '0',
+    ease: 'Power3.in'
+        // ease: 'slow.easeInOut'
+});
+document.querySelectorAll('.skills-box .percentage').forEach(d => {
+    console.log(d.getAttribute('data-w'))
+    gsap.to(d, {
+        scrollTrigger: {
+            trigger: ".skills-box",
+            start: "top center",
+        },
+        delay: 0,
+        duration: 1,
+        width: d.getAttribute('data-w'),
+        ease: 'Power3.in'
+            // ease: 'slow.easeInOut'
+    });
+});
+//animate works items
+gsap.to([".work-item", ".services-section .grid div:nth-child(2)", ".services-section .grid div:nth-child(5)"], {
+    scrollTrigger: {
+        trigger: ".works-section",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 2,
+    opacity: 1,
+    y: '0',
+    ease: 'Power3.in'
+        // ease: 'slow.easeInOut'
+});
+//animate from right to left the 3 rd and the 6th service div
+gsap.to([".services-section .grid div"], {
+    scrollTrigger: {
+        trigger: ".services-section",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 1,
+    opacity: 1,
+    x: '0',
+    ease: 'Power2.in'
+        // ease: 'slow.easeInOut'
+});
+//animate the 2nd and 5th service div
+gsap.to([".services-section .grid div:nth-child(2)", ".services-section .grid div:nth-child(5)"], {
+    scrollTrigger: {
+        trigger: ".services-section",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 1,
+    opacity: 1,
+    y: '0',
+    ease: 'Power3.in'
+        // ease: 'slow.easeInOut'
+});
+//animate from left to right the 1st and 4th service div
+gsap.to([".services-section .grid div:nth-child(1)", ".services-section .grid div:nth-child(4)"], {
+    scrollTrigger: {
+        trigger: ".services-section",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 1,
+    opacity: 1,
+    x: '0',
+    ease: 'Power1.in'
+        // ease: 'slow.easeInOut'
+});
+//animate the blog section
+gsap.to([".blog-section div div"], {
+    scrollTrigger: {
+        trigger: ".blog-section",
+        start: "top center",
+    },
+    delay: 0,
+    duration: 1,
+    opacity: 1,
+    scale: '1',
+    ease: 'Power1.in'
+        // ease: 'slow.easeInOut'
+});
