6f43493bab233bdbee78e4bf720ecd65e6642493
ayatamine <ayatir04@gmail.com>
2022-04-29 12:16:51 +0100

    3 nd optimization in the ui + documentation

---
+++ public/contact.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+// Define Recipient name and emaiil
+define( "RECIPIENT_NAME", "John Doe" );
+define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+
+
+// Read the form values
+$success = false;
+$userName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Message:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $userName && $senderEmail && $message) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $username . "";
+  $msgBody = " Last Name: " . $userName . " Email: " . $senderEmail . " Message: " . $message . "";
+  $success = mail( $recipient, $headers, $msgBody );
+
+  //Set Location After Successsfull Submission
+  header('Location: index.html?message=Successfull');
+}
+
+else{
+	//Set Location After Unsuccesssfull Submission
+  	header('Location: contact.html?message=Failed');
+}
+
+?>
--- public/js/main.js
+++ public/js/main.js
@@ -7,18 +7,23 @@ function handlePreloader() {
         }, 5000);
     });
 }
-// handlePreloader();
+handlePreloader();
 
+//update the selected circle for the left side menu when scroll
 function updateList() {
+    //there is only one H1 tag in the document (Home page)
+    //sort all H2 Tags based on their distance from the first H1 Tag
     const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
         return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
     });
-
+    //remove selected-circle class from all links of the left side navigatin
     document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+    //set the selected-circle class to the selected link of the left side navigation
     document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
 }
-
+//update selected navigation link when click
 updateList();
+//update selected navigation when scroll
 window.addEventListener('scroll', () => {
     updateList();
 })
@@ -26,8 +31,7 @@ window.addEventListener('scroll', () => {
 //open and close the mobile menuu
 function switchMenu(type) {
     let menu = document.querySelectorAll('.mobile-menu')[0];
-
-
+    //define animation attributes
     const extendClipPath = [
         { clipPath: 'circle(25px at calc(100% - 45px) 45px)' },
         { clipPath: 'circle(75%)' }
@@ -38,15 +42,17 @@ function switchMenu(type) {
     ];
 
     const newspaperTiming = {
-        duration: 300,
-        iterations: 1,
-    }
+            duration: 300,
+            iterations: 1,
+        }
+        //open the mobile menu
     if (type == 'open') {
         menu.classList.remove('hidden');
         menu.classList.add('visible');
         menu.animate(extendClipPath, newspaperTiming);
         return;
     }
+    //close the mobile menu
     menu.classList.remove('visible');
     menu.animate(resizeClipPath, newspaperTiming);
     setTimeout(() => {
@@ -54,24 +60,31 @@ function switchMenu(type) {
     }, 250);
 
 }
+
 //hide mobile menu when choose a link from it
 document.querySelectorAll('.mobile-menu li a').forEach(link => {
     link.addEventListener('click', () => switchMenu('close'))
 });
-//animation on scroll
+
+//[----Start the animation using GSAP Library----]
+
+//register global ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger)
+
+//animate the Image + caption of the About section
 gsap.to([".featured-img", ".caption"], {
     scrollTrigger: {
         trigger: ".about-me-section",
         start: "top top",
     },
-    delay: 0,
-    duration: 1.5,
-    opacity: 1,
-    right: '0%',
-    ease: 'Power3.in'
-        // ease: 'slow.easeInOut'
+    delay: 0, //set animation delay == 0
+    duration: 1.5, //set animatin duration to 1.5 s
+    opacity: 1, //set opacity == 1
+    right: '0%', //set right == 0
+    ease: 'Power3.in' //set animation to ease in type Power3
 });
+
+//animate the resume timeline section
 gsap.to([".resume-box .timeline"], {
     scrollTrigger: {
         trigger: ".resume-box",
@@ -84,6 +97,7 @@ gsap.to([".resume-box .timeline"], {
     ease: 'Power3.in'
         // ease: 'slow.easeInOut'
 });
+//animate the skills boxes percentages of the Skills section
 document.querySelectorAll('.skills-box .percentage').forEach(d => {
     gsap.to(d, {
         scrollTrigger: {
@@ -97,7 +111,8 @@ document.querySelectorAll('.skills-box .percentage').forEach(d => {
             // ease: 'slow.easeInOut'
     });
 });
-//animate works items
+
+//animate works items + 2nd and 5th services items
 gsap.to([".work-item", ".services-section .grid div:nth-child(2)", ".services-section .grid div:nth-child(5)"], {
     scrollTrigger: {
         trigger: ".works-section",
@@ -108,9 +123,9 @@ gsap.to([".work-item", ".services-section .grid div:nth-child(2)", ".services-se
     opacity: 1,
     y: '0',
     ease: 'Power3.in'
-        // ease: 'slow.easeInOut'
 });
-//animate from right to left the 3 rd and the 6th service div
+
+//animate from right to left the 3 rd and the 6th services items
 gsap.to([".services-section .grid div"], {
     scrollTrigger: {
         trigger: ".services-section",
@@ -123,7 +138,8 @@ gsap.to([".services-section .grid div"], {
     ease: 'Power2.in'
         // ease: 'slow.easeInOut'
 });
-//animate the 2nd and 5th service div
+
+//animate the 2nd and 5th services items
 gsap.to([".services-section .grid div:nth-child(2)", ".services-section .grid div:nth-child(5)"], {
     scrollTrigger: {
         trigger: ".services-section",
@@ -134,9 +150,9 @@ gsap.to([".services-section .grid div:nth-child(2)", ".services-section .grid di
     opacity: 1,
     y: '0',
     ease: 'Power3.in'
-        // ease: 'slow.easeInOut'
 });
-//animate from left to right the 1st and 4th service div
+
+//animate from left to right the 1st and 4th services items
 gsap.to([".services-section .grid div:nth-child(1)", ".services-section .grid div:nth-child(4)"], {
     scrollTrigger: {
         trigger: ".services-section",
@@ -149,6 +165,7 @@ gsap.to([".services-section .grid div:nth-child(1)", ".services-section .grid di
     ease: 'Power1.in'
         // ease: 'slow.easeInOut'
 });
+
 //animate the blog section
 gsap.to([".blog-section div div"], {
     scrollTrigger: {
@@ -160,5 +177,4 @@ gsap.to([".blog-section div div"], {
     opacity: 1,
     scale: '1',
     ease: 'Power1.in'
-        // ease: 'slow.easeInOut'
 });
